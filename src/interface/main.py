4b5fb09e27611b169e5d00ec04a951771e672411
lucile bessac <lucilebessac@free.fr>
2025-03-12 00:16:27 +0100

    feat; premiers essais de squelette d'API

--- src/interface/main.py
+++ src/interface/main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+
+from CNN.occitanCNN import OccitanCNN ## PB DE CHEMIN À RÉSOUDRE
+from CNN.dataset import tokenizer_occitan, vectorizer_phrase, padding_liste_phrases, tensorizer_phrase, charger_fasttext # PB DE CHEMIN À RÉSOUDRE
+from post_input import TextInput
 import torch
 
 #Initialisation de FastAPI
@@ -8,9 +12,11 @@ app = FastAPI()
 #Cherche si gpu ou cpu -> optimise 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+# Importer le modèle pretrained
+cnn_model = OccitanCNN().to(device)
+cnn_model.load_state_dict(torch.load("trained_OccitanCNN.pth")) # PATH A CHANGER
 
-
-
+vec_model = charger_fasttext()
 
 #Permet de tester si l’API fonctionne.
 @app.get("/")
@@ -19,18 +25,19 @@ def root():
 
 
 #Requête de l'utilisateur (POST)
-#@app.post("/prediction/")
+@app.post("/prediction/")
+def prediction(data: TextInput): # TextPost est un objet qui contient un attribut text / il faut le définir
 
-    #transformation dans un format qui convient à notre modèle
-        #tokeniser
-        #vectoriser
-        #padding
+    # transformation dans un format qui convient à notre modèle
+    input_tokenise = tokenizer_occitan(data.text)
+    input_vectorise = vectorizer_phrase(input_tokenise, vec_model, 56) # max_len = 56, à récupérer automatiquement / vérifier
+    input_vectorise_padded = padding_liste_phrases(input_vectorise, 56) # max_len = 56, à récupérer automatiquement / vérifier
+    input_tensor = tensorizer_phrase(input_vectorise_padded, torch.float32)
 
-    #plus ou moins comme la fonction evaluate_model
+    # prédiction
+    prediction = cnn_model(input_tensor)
 
-
-
-#return {"text": data.text, "prediction": label}
+    return {"text": data.text, "prediction": prediction}
 
 
 
